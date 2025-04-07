ad962b96a8f2a1f985362e56aa6b36d1a1de1571
Delight-Fela-Steve <dfelastevegit@gmail.com>
2025-04-07 09:46:20 +0100

    feat: modify init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE sharedappdb;
 \c sharedappdb;
 
-CREATE TABLE devs (
+CREATE TABLE IF NOT EXISTS devs (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50)
 );
 
-INSERT INTO devs (name) VALUES ('Flask Developer'), ('Node Developer'), ('Shared DB User');
+INSERT INTO devs (name) 
+SELECT unnest(ARRAY['Flask Developer', 'Node Developer', 'Shared DB User'])
+WHERE NOT EXISTS (SELECT 1 FROM devs);
